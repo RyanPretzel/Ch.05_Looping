e530cfade58bcef4f80984f0e4ffb081e37140f5
muetzelrya <muetzelrya@urbandaleschools.com>
2020-10-16 10:29:43 -0500

    5.2 probably done

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -15,20 +15,49 @@ import random
 
 user = 0
 end = False
+user_count = 0
+total_count = 0
 
-print('This is a game of rock, paper, scissors.  Enter 1 for rock, 2 for paper, or 3 for scissors.')
+def user_win():         #defining fuction for when the player wins
+    print('The player wins!')
+    global user_count
+    user_count += 1
+
+print('This is a game of rock, paper, scissors.  Enter 1 for rock, 2 for paper, or 3 for scissors.\nTo end the game, enter 0.')
 while end == False:
-    user = input('Please Enter: ')
+    user = int(input('Please Enter: '))
     comp = random.randint(1, 3)
-    print('User is '+str(user))  #Printing for debug
-    print('Comp is '+str(comp))
-    if int(user) == comp:      #if both players choose the same comp, it ties
-        print('Tie!')
-    elif int(user) == 1 and comp == 3:
-        print('rock')
-    elif int(user) == 2:
-        print('paper')
-    elif int(user) == 3:
-        print('scissors')
-    else:
+
+    if comp == 1:          #assigns comp's number to the corresponding choice
+        comp_str = 'Rock'
+    elif comp == 2:
+        comp_str = 'Paper'
+    elif comp == 3:
+        comp_str = 'Scissors'
+
+    if user == 1:          #assigns user's number to the corresponding choice
+        user_str = 'Rock'
+    elif user == 2:
+        user_str = 'Paper'
+    elif user == 3:
+        user_str = 'Scissors'
+    else:               #if the user doesnt make a valid response, the program ends
         end = True
+        print('Thank you for playing!')
+        continue
+    #Tells the user their own choice and the computer's choice
+    print('\nThe player has chosen '+user_str+', and the computer has chosen '+comp_str+'.')
+
+    total_count += 1
+    if user == comp:    #Does statement comparison to detirmine who wins
+        print('Tie!')
+    elif user == 2 and comp == 1:
+        user_win()
+    elif user == 3 and comp == 2:
+        user_win()
+    elif user == 1 and comp == 3:
+        user_win()
+    else:       #if it isnt a tie, and the user doesnt win, then the computer must win
+        print('The computer wins!')
+
+    print('\nThe player has won '+str(user_count)+' of '+str(total_count)+' games.')
