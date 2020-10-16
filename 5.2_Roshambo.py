aff6172e09b99a1d151c970cfbc06fc3f86bd3fd
muetzelrya <muetzelrya@urbandaleschools.com>
2020-10-16 09:55:33 -0500

    5.2 started still a mess

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -11,13 +11,24 @@ When the user quits print a win/loss record
 
 '''
 
+import random
 
+user = 0
+end = False
 
-
-
-
-
-
-
-
-
+print('This is a game of rock, paper, scissors.  Enter 1 for rock, 2 for paper, or 3 for scissors.')
+while end == False:
+    user = input('Please Enter: ')
+    comp = random.randint(1, 3)
+    print('User is '+str(user))  #Printing for debug
+    print('Comp is '+str(comp))
+    if int(user) == comp:      #if both players choose the same comp, it ties
+        print('Tie!')
+    elif int(user) == 1 and comp == 3:
+        print('rock')
+    elif int(user) == 2:
+        print('paper')
+    elif int(user) == 3:
+        print('scissors')
+    else:
+        end = True
