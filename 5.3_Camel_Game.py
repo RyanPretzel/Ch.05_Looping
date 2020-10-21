aa488f9eaf2d0c8cfe4e500949ad0a09593c84d4
muetzelrya <muetzelrya@urbandaleschools.com>
2020-10-21 09:41:33 -0500

    5.3 basic loop done

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -6,14 +6,27 @@ The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi boo
 
 Ok so here is the situation
 You are an ADC, and there is farm in the bot lane
-However, the Mid laner is missing, and he is chasing you
+However, the Mid laner is missing, and he is chasing you and your support.
 Try to escape to your fountain with the most CS possible
 '''
 
+import random
+
 done = False
+cs = 0
+distance_fountain = 0
+support_tiredness = 0
+zed = random.randint(10,15)
 
-
-
+while done == False:
+    print('\nA) Drink from your canteen\nB) Moderate Speed\nC) Full Speed\nD) Rest\nE) Status Check\nG) Greed for the CS\nQ) Quit')
+    choice = input('\nPlease choose your action: ')
+    if choice.upper() == 'Q': #End game if user wants to
+        done = True
+        continue
+    else:
+        print('Zed has caught up')
+    print(str(zed))
 
 
 
