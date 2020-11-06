9df89a010b61f20358fc6ca4324718b5f04be493
muetzelrya <muetzelrya@urbandaleschools.com>
2020-11-06 14:24:25 -0600

    5.3 Minor bug fixes
    Patch notes:
    Changes to how Zed's distance is calculated

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -18,8 +18,8 @@ cs = 0
 mana_pots = 2
 mana = 550
 support_unhappiness = 0
-zed_dist = random.randint(18, 25)
-support = False
+zed_dist = random.randint(25, 28)
+support = True
 tower = False
 turn = 0
 flash = True
@@ -49,17 +49,20 @@ def zed_turn():
         time.sleep(.75)
 
     if turn >= 1: # # if the turn is sucessful, then kill zed and win the game
-        print('\n\nYou sucessfully kill Zed!  Congratulations!')
+        print('\n\nYou sucessfully kill Zed!  You win!')
+        print('\n[All] Oogabooga23 (Zed): adc diff')
     if turn < 1:
         print('\n\nYou were not strong enough, so Zed killed you.  Try getting more CS next time.')
     done = True
 
 
 # # Instructions on how to play the game
-print('Welcome to league of legends!  You made a mistake coming here.')
+print('\nWelcome to league of legends!  You made a mistake coming here.')
 print("Today you will be playing an ADC, which means you need CS (minion kills) in order to do anything. Focus on farming and don't get killed.")
-print('The mid lane assassin is Zed, and he is missing.  Make sure you keep enough distance from him.')
+print('The mid lane assassin is Zed, and he is coming to kill you.  Make sure you keep enough distance from him.  About 20-30 units away is ideal.')
 print('Make sure you keep your support happy, they can leave you.')
+print('Mana can be used to get cs faster, but make sure you have enough.')
+print('In order to win, you must gather lots of cs so you can do damage, then you can turn and kill Zed.')
 
 while done == False:
     if support == True:
@@ -72,30 +75,29 @@ while done == False:
         continue
     elif choice.upper() == 'A':     # #if user drinks mana potion
         if mana_pots >= 1:
-            print('\nYou drink a mana potion and receive 100 mana.  You now have '+str(mana)+' mana.')
             mana += 100
             mana_pots -= 1
+            print('\nYou drink a mana potion and receive 100 mana.  You now have '+str(mana)+' mana.')
             continue
         else:
             print('You do not have enough mana potions to do that.')
             continue
     elif choice.upper() == 'B':     # #if user runs at moderate speed
         temp_distance = random.randint(10,15)
-        zed_dist += temp_distance - random.randint(10,15)
-        support_unhappiness += random.randint(1,2)
+        zed_dist += random.randint(3, 4)
+        support_unhappiness += 1
         print('\nYou are running away from Zed.  You are now '+str(zed_dist)+' units away from Zed')
-        if random.randint (1,2) == 1: # #random event to find extra cs
-            cs += random.randint(1,2)
-            print('You found more CS! You now have '+str(cs)+' cs.')
+        if random.randint (1,3) == 1: # #random event to find extra cs
+            cs += random.randint(1, 2)
+            print("You randomly 'found' more CS! You now have "+str(cs)+" cs. The top laner is not happy.")
         if random.randint(1,3) == 1: # #random event to find a tower
             tower = True
             print('You are now under a protective tower')
         else:
             tower = False
     elif choice.upper() == 'C':  # # if user runs at high speed
-        temp_distance = random.randint(19, 24)
-        zed_dist += temp_distance - random.randint(10, 15)
-        support_unhappiness += random.randint(1, 3)
+        zed_dist += random.randint(8, 12)
+        support_unhappiness += random.randint(1, 2)
         print('\nYou are quickly running away from Zed.  You are now '+str(zed_dist)+' units away from Zed')
         if random.randint(1,3) == 1:   # #random event to find tower
             tower = True
@@ -116,13 +118,17 @@ while done == False:
             print('Your support is gone.')
             continue
     elif choice.upper() == 'F':     # #user quickly gets the wave, but they use mana
-        temp_mana = random.randrange(130, 161, 15)
-        mana -= temp_mana
-        support_unhappiness += 1
-        print('\nYou quickly clear the wave, but you use '+str(temp_mana)+' mana.  You now have '+str(mana)+' mana.')
-        cs += random.randint(5,7)
-        zed_dist -= random.randint(10,15)
-        print('\nYou now have '+str(cs)+' cs.')
+        if mana >= 160:
+            temp_mana = random.randrange(115, 146, 15)
+            mana -= temp_mana
+            support_unhappiness += 1
+            print('\nYou quickly clear the wave, but you use '+str(temp_mana)+' mana.  You now have '+str(mana)+' mana.')
+            cs += random.randint(5,7)
+            zed_dist -= random.randint(10,14)
+            print('\nYou now have '+str(cs)+' cs.')
+        else:
+            print('You do not have enough mana to do that.')
+            continue
     elif choice.upper() == 'G':     # #slowly greed for the wave
         cs += random.randint(5,7)
         zed_dist -= random.randint(13,17)
@@ -145,7 +151,38 @@ while done == False:
     else:
         print('That is not a valid response. ')
 
-    # #random event to find a camp or a mana potion
+        # #ways to die
+    if zed_dist <= 0:
+        print('Zed has jumped onto you.')
+        if flash == True:
+            print('\nWould you like to turn (T) on zed? Or do you want to flash (F) away?')
+            choice = input('Enter T or F to choose: ')
+            if choice.upper() == 'T':
+                zed_turn()
+                continue
+            elif choice.upper() == 'F':
+                zed_dist = 18
+                flash = False
+                print('\nYou used your flash to escape.  You are now ' + str(zed_dist) + ' units away from zed')
+            elif choice.upper() == 'D':
+                print("\nFlash goes on F, but I'll let you get away this time.")
+                zed_dist = 18
+                flash = False
+                print('\nYou used your flash to escape.  You are now ' + str(zed_dist) + ' units away from zed')
+            else:
+                print('\nYou did not pick a valid response.  Zed kills you.')
+                done = True
+                continue
+        else:
+            print('\nYou do not have flash, so you are forced to turn on Zed.')
+            zed_turn()
+            continue
+    elif zed_dist <= 16:
+        print('Zed is too close for comfort.')
+        if tower == True:
+            print('You are under a protective tower.')
+
+    # #random event to find a camp, cs, or a mana potion
     if random.randint(1,6) == 1 and tower == False:
         print('\nYou find a camp of raptors nearby in the jungle.  Would you like to take it? (Y/N)')
         choice = input('Enter choice here (Y/N): ')
@@ -174,6 +211,7 @@ while done == False:
         if choice.upper() == 'Y':
             cs += random.randint(5,7)
             zed_dist -= random.randint(9,14)
+            print('\nYou now have '+str(cs)+' cs.')
         elif choice.upper() == 'N':
             print('\nYou did not take the mid wave.')
         else:
@@ -182,38 +220,12 @@ while done == False:
         mana_pots += 1
         print('You found a mana potion!')
 
-    # #ways to die
-    if zed_dist <= 0:
-        print('Zed has jumped onto you.')
-        if flash == True:
-            print('\nWould you like to turn (T) on zed? Or do you want to flash (F) away?')
-            choice = input('Enter T or F to choose: ')
-            if choice.upper() == 'T':
-                zed_turn()
-                continue
-            elif choice.upper() == 'F':
-                zed_dist = 18
-                flash = False
-                print('\nYou used your flash to escape.  You are now '+str(zed_dist)+' units away from zed')
-            else:
-                print('\nYou did not pick a valid response.  Zed kills you.')
-                done = True
-                continue
-        else:
-            print('You do not have flash, so you are forced to turn on Zed.')
-            zed_turn()
-            continue
-    elif zed_dist <= 14:
-        print('Zed is too close for comfort.')
-        if tower == True:
-            print('You are under a protective tower.')
-
 
     if support == True:     # #If the support is still there
         if support_unhappiness >= 16:       # #if he is too unhappy he is just gone
             support = False
             print('\nYou support has left you.')
-        elif support_unhappiness >= 10:     # #if he is kinda unhappy there is a chance that he leaves
+        elif support_unhappiness >= 12:     # #if he is kinda unhappy there is a chance that he leaves
             if random.randint(1,4) == 1:
                 support = False
                 print('Your support has left you.')
